7a0e663680f24ecd3dd33b946a263013bde97942
abhishek2524 <66551338+abhishek2524@users.noreply.github.com>
2021-02-02 23:02:10 +0530

    updated logic for canvas plot

--- tasks.js
+++ tasks.js
@@ -3,55 +3,36 @@ const {options,createDataset} = require('./common/chart-config');
 const { response } = require('express');
 var inside = require('point-in-polygon');
 
-const getParameterByIndex = (data,index1,index2)=>{
-    let new_dataset = []
-    data.map((data)=>{
-        new_dataset.push({x:data[index1],y:data[index2]})
-    })
-    return new_dataset
-}
-
 const task1 = ()=>{
-    let resData = {};
-    let datasets = getParameterByIndex(dataset,0,1);
-    let data = createDataset(datasets,'white',labels="dataset")
-    resData["datasets"] = [data]
-    response["options"] = options;
-    response['data'] = resData;
-    return response;
+    let plotData = [];
+    dataset.map((res)=>{
+        plotData.push([res[0],res[1],'white'])
+    })
+    return plotData;
 }
 
 const task2 = ()=>{
-    let response = {}
     let polygonList = []
     let notPolygonList = []
-    let resData = {}
+    let plotData = []
     const polygon = polygon1;
     let new_dataset = []
     dataset.map((data)=>{
-        new_dataset.push([data[0],data[1]])
+        new_dataset.push(data)
     })
     new_dataset.map((data)=>{
-        if(inside(data, polygon)){
-            polygonList.push(data)
+        if(inside([data[0],data[1]], polygon)){
+            polygonList.push([data[0],data[1],'red'])
         }else{
-            notPolygonList.push(data)
+            notPolygonList.push([data[0],data[1],'white'])
         }
     })
-    const isPolyData = getParameterByIndex(polygonList,0,1)
-    const notPolyData = getParameterByIndex(notPolygonList,0,1)
-    let data1 = createDataset(isPolyData,'red',labels="dataset1")
-    let data2 = createDataset(notPolyData,'white',labels="dataset2")
-
-    resData["datasets"] = [data1,data2]
-    response["options"] = options;
-    response['data'] = resData;
-    return response;
+    plotData = polygonList.concat(notPolygonList);
+    return plotData;
 }
 
 const task3 = ()=>{
-    let response = {}
-    let resData = {}
+    let polyData = []
     const polygon = polygon1;
     let new_dataset = []
     dataset.map((data)=>{
@@ -59,18 +40,13 @@ const task3 = ()=>{
             new_dataset.push(data)
         }
     })
-    const isPolyData = getParameterByIndex(new_dataset,3,2)
-    let data = createDataset(isPolyData,'red',labels="dataset")
-    resData["datasets"] = [data]
-    response["options"] = options;
-    response['data'] = resData;
-
-    return response;
+    polyData = new_dataset.map((res)=>{
+        return [res[3],res[2],'red']
+    })
+    return polyData;
 }
 
 const task4 = ()=>{
-    let response = {}
-    let resData = {}
     let new_dataset = []
     let data = []
     dataset.map((data)=>{
@@ -80,20 +56,15 @@ const task4 = ()=>{
     })
     new_dataset.map((res)=>{
         if(inside([res[3],res[2]],polygon2)){
-            data.push(res)
+            data.push([res[3],res[2],'green'])
         }
     })
-    const isPolyData = getParameterByIndex(data,3,2)
-    data = createDataset(isPolyData,'green',label="dataset")
-    resData["datasets"] = [data]
-    response["options"] = options;
-    response['data'] = resData;
-
-    return response;
+    return data;
 }
 
 const bonusTask = ()=>{
     let response = {}
+    const data = []
     let polygonList = []
     let notPolygonList = []
     let secPolygonList = []
@@ -117,18 +88,16 @@ const bonusTask = ()=>{
         }
     })
 
-    const notPolyData = getParameterByIndex(notPolygonList,0,1)
-    let data1 = createDataset(notPolyData,'white',labels="dataset1")
-
-    const polyData = getParameterByIndex(polygonList,0,1)
-    let data2 = createDataset(polyData,'red',labels="dataset2")
-
-    const secPolyData = getParameterByIndex(secPolygonList,0,1)
-    let data3= createDataset(secPolyData,'green',labels="dataset3")
-
-    resData["datasets"] = [data1,data2,data3]
-    response["options"] = options;
-    response['data'] = resData;
-    return response;
+    notPolygonList.map((res)=>{
+        data.push([res[0],res[1],'white']);
+    });
+    polygonList.map((res)=>{
+        data.push([res[0],res[1],'red']);
+    });
+    secPolygonList.map((res)=>{
+        data.push([res[0],res[1],'green']);
+    });
+    
+    return data;
 }
 module.exports = {task1,task2,task3,task4,bonusTask}
